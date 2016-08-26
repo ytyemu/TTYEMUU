4ac93fced590bd8f666c4d093d5c334d3dea1194
mac <mac@67887i.local>
2016-08-26 15:51:35 +0800

    初始化

---
+++ gittest/gittest/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  gittest
+//
+//  Created by mac on 16/8/26.
+//  Copyright © 2016年 zhiyou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"new");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
